3f2832df3ed014c07d69e2f43e2f4656d4f30662
rocky <983272765@qq.com>
2015-10-19 10:34:46 +0800

    Initial Commit

---
+++ HLLContactsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLContactsDemo
+//
+//  Created by apple on 15/10/19.
+//  Copyright © 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLContactsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLContactsDemo
+//
+//  Created by apple on 15/10/19.
+//  Copyright © 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
